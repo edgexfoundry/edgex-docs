551a057ecb86af4c53dbee65fdd961dc4c683584
Ernesto Ojeda <ernesto.ojeda@intel.com>
2019-07-08 11:53:37 -0700

    refactor Dockerfile to use new base image with dependencies
    Signed-off-by: Ernesto Ojeda <ernesto.ojeda@intel.com>

--- build.sh
+++ build.sh
@@ -2,13 +2,14 @@
 set -e
 
 # Build image (copying in documentation sources)
+# default to master branch, can be overriden
+build_stream=${1:-master}
 
-docker build -t doc-builder:latest -f Dockerfile.build .
+docker build -t doc-builder:latest -f Dockerfile.build --build-arg STREAM=${build_stream} .
 rm -rf _build
 mkdir _build
 
 # Build documentation in container
-
 docker run --rm -v "$(pwd)"/_build:/docbuild/_build doc-builder:latest
 
 
